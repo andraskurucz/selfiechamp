0bd67b155fb2935c0dfc989b5c637f38f906ea99
Szabolcs Varadi <szabolcs_varadi@epam.com>
2015-02-10 17:58:32 +0100

    image upload

---
+++ backend/routes/images.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Szabolcs_Varadi on 2015-02-10.
+ */
+
+var db = require('../db');
+var auth = require('../utils/authentication');
+var fs = require('fs');
+
+function create(req, res, next) {
+    for (var i = 0; i < req.files.length; i++) {
+        var file = req.files.item(i);
+        fs.writeFileSync('../public/images/' + file.name, file, );
+    }
+
+}
+
+/* nothing is exported yet don't affects the routes */
